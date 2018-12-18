4d1a1518255a6833e9e1c3ae56808e13790d9fb6
irakli iakobishvili <irakli.iakobishvili@gmail.com>
2018-12-18 20:48:39 +0400

    full Screen Menu (html+css+js) / WORKS!

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,12 @@ const Tinyone = {
 
     topNavBtn: document.querySelector('#topNavBtn'),
     menuPage:  document.querySelector('#menuPage'),
+
+    menuLinks: document.querySelectorAll('.fullScreenMenu__list__item__link'),
+    activeClass: 'fullScreenMenu__list__item__link--active',
+    container: document.querySelector('#container'),
     changePage: function(e) {
+        Tinyone.container.classList.add('pageScrollAnimation');
         e.preventDefault();
         // let totalPage = 6;
         let vh = window.innerHeight;
@@ -20,20 +25,52 @@ const Tinyone = {
             if (e.deltaY < 0) {            
                 if(Tinyone.topValue !== 0) {
                     Tinyone.topValue += vh;
+                    Tinyone.i--;
                     Tinyone.container.style.top = `${Tinyone.topValue}px`; 
-                }
+                }                
+                
+                Tinyone.menuLinks.forEach((el) => {
+                    el.classList.remove(Tinyone.activeClass);
+                });
+                Tinyone.menuLinks[Tinyone.i].classList.add(Tinyone.activeClass);
             }
             if (e.deltaY > 0) {            
                 if(Tinyone.topValue != -`${(Tinyone.totalPage.length-1)*vh}`) {
                     Tinyone.topValue += -vh;
+                    Tinyone.i++;
                     Tinyone.container.style.top = `${Tinyone.topValue}px`;
                 }
+
+                Tinyone.menuLinks.forEach((el) => {
+                    el.classList.remove(Tinyone.activeClass);
+                });
+                Tinyone.menuLinks[Tinyone.i].classList.add(Tinyone.activeClass);
             }
         }
     },
-    fullScreenMenu: function(e) {
-        // Tinyone.topNavBtn.classList.toggle('showFullScreenMenu');
+    fullScreenMenu: function() {
+        Tinyone.topNavBtn.classList.toggle('fullScreenBtnStyle');
         Tinyone.menuPage.classList.toggle('showFullScreenMenu');
+        // Tinyone.container.classList.add('pageScrollAnimation');
+    },
+    clickingLinks: function(e) {
+        Tinyone.container.classList.remove('pageScrollAnimation');
+        for(let i = 0; i < Tinyone.menuLinks.length; i++) {
+            Tinyone.menuLinks[i].setAttribute('data-index',i);
+            Tinyone.menuLinks[i].setAttribute('data-vh',-i*window.innerHeight);
+        }
+        e.preventDefault();
+        Tinyone.menuLinks.forEach((el) => {
+            el.classList.remove(Tinyone.activeClass);
+        });
+        e.target.classList.add(Tinyone.activeClass);
+
+        Tinyone.i = Number(e.target.getAttribute('data-index'));
+        Tinyone.topValue = Number(e.target.getAttribute('data-vh'));
+        Tinyone.container.style.top = `${Tinyone.topValue}px`;
+        // console.log(e.target);
+        
+        Tinyone.fullScreenMenu();
     }
 }
 
@@ -56,4 +93,9 @@ Tinyone.topNavBtn.addEventListener('click',Tinyone.fullScreenMenu);
 Tinyone.downArrow.addEventListener('click',function(){
     Tinyone.topValue += -window.innerHeight;
     Tinyone.container.style.top = `${Tinyone.topValue}px`;
+});
+
+Tinyone.menuLinks.forEach((el) => {
+    // el.classList.remove(Tinyone.activeClass);
+    el.addEventListener('click',Tinyone.clickingLinks);
 });
